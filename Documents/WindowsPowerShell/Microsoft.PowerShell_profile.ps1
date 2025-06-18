10efe4280e54b39aa8de572aff84ac769727763f
Голубь <66267734+Golyb-q@users.noreply.github.com>
2025-06-18 00:13:23 +0000

    laptop

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,26 @@
-Set-Alias open explorer
 Set-Alias make mingw32-make
+Set-Alias open explorer
+
+function torproxy {
+    param (
+        [ValidateSet("on", "off")]
+        [string]$action
+    )
+
+    switch ($action) {
+        "on" {
+            $env:ALL_PROXY = "socks5h://127.0.0.1:9150"
+            Write-Host "Tor proxy on"
+        }
+        "off" {
+            Remove-Item Env:ALL_PROXY -ErrorAction SilentlyContinue
+            Write-Host "Tor proxy off"
+        }
+        default {
+            Write-Host "Использование: torproxy on|off"
+        }
+    }
+}
 
 function Get-UniqueFilename {
     param (
@@ -18,17 +39,48 @@ function Get-UniqueFilename {
     return $uniqueFilepath
 }
 
-function tomp3 {
+function toflac {
     param (
-        [string]$inputFile
+        [string]$inputFile,
+        [string]$coverImage = $null
     )
-    if (-not $inputFile) {
-        Write-Output "Использование: tomp3 <путь к файлу>"
+    if (-not (Test-Path $inputFile)) {
+        Write-Host "Использование: toflac <путь к аудиофайлу> [-cover <путь к обложке>]. Конвертер в FLAC"
         return
     }
+
+    $outputFile = [System.IO.Path]::ChangeExtension($inputFile, ".flac")
+    $outputFile = Get-UniqueFilename $outputFile
+
+    if ($coverImage -and (Test-Path $coverImage)) {
+        ffmpeg -i "$inputFile" -i "$coverImage" -compression_level 12 -map 0 -map 1 `
+               -c:a flac -disposition:v:0 attached_pic "$outputFile"
+    }
+    else {
+        ffmpeg -i "$inputFile" -compression_level 12 -c:a flac "$outputFile"
+    }
+}
+function tomp3 {
+    param (
+        [string]$inputFile,
+        [string]$coverImage = $null
+    )
+    if (-not (Test-Path $inputFile)) {
+        Write-Output "Использование: tomp3 <путь к файлу> [-cover <путь к обложке>]"
+        return
+    }
+
     $outputFile = [System.IO.Path]::ChangeExtension($inputFile, ".mp3")
     $outputFile = Get-UniqueFilename $outputFile
-    ffmpeg -i "$inputFile" -b:a 320k "$outputFile"
+
+    if ($coverImage -and (Test-Path $coverImage)) {
+        ffmpeg -i "$inputFile" -i "$coverImage" -map 0 -map 1 -c:a libmp3lame -b:a 320k `
+               -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" `
+               "$outputFile"
+    }
+    else {
+        ffmpeg -i "$inputFile" -b:a 320k "$outputFile"
+    }
 }
 
 function tomp4 {
